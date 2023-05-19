f0122aa9021e11012d2d6ebaec6b765e1675ba86
oluwatobimaxwell <tobisholanke@gmail.com>
2023-05-20 00:00:12 +0300

    first react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Welcome to CodeForge First React App</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - JSX
         </a>
       </header>
     </div>
   );
 }
-
 export default App;
+
+export function TobiApp() {
+  return <div
+  className="tobi-app"
+  >This is the Tobi Application</div>;
+}
